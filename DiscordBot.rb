840563f1b8cb47d5e2278884ff11244487d3d613
David S <dsolymosi@gmail.com>
2016-09-09 10:10:24 -0700

    perm fix for testing server
    coldieBot wants freedom

--- DiscordBot.rb
+++ DiscordBot.rb
@@ -11,8 +11,9 @@ require_relative 'DBMisc'
 
 	#Perms
 	@bot.set_user_permission(109517845678809088, 	101)	#Zeph
-   #@bot.set_user_permission(110907958824538112, 	101)	#coldie
-   #Not yet buddy ;) Gotta talk to Toast first
+	if @bot.profile.username == "coldieBot" then #for testing
+   		@bot.set_user_permission(110907958824538112, 	101)	#coldie
+    end
 	@bot.set_role_permission(210829888066813965,	100)	#Owner
 	@bot.set_role_permission(210832404636631040,	 99)	#Admin
 	@bot.set_role_permission(210830743771938817, 	 50)	#Mod
@@ -32,8 +33,7 @@ end
 #Eval Command
 @bot.command(:eval, description: "Run any code you want!!!", usage: "eval *Literally any code*", permission_level: 101) do |event, *code|
 	p Time.now.to_s + " " + event.user.name
-  break unless event.user.id == 109517845678809088 #|| event.user.id == 110907958824538112  # Replace number with your ID
-  													#Toasty first
+  break unless event.user.id == 109517845678809088 || (@bot.profile.username == "coldieBot" && event.user.id == 110907958824538112)  # Replace number with your ID
   begin
     eval code.join(' ')
   rescue
