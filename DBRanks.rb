39b4e5233669d78799a51cbee5228fad1d11d1bc
Tucker Sneed <tuckerdsneed@gmail.com>
2016-09-27 17:20:38 -0700

    Rank Comments 1
    Just clarifying what each part of DBRanks.rb does.

--- DBRanks.rb
+++ DBRanks.rb
@@ -1,8 +1,10 @@
 require_relative 'DBCommon'
 require 'RubyMinecraft'
 
+/* @ranks is an instance hash of the ranks used by RRR
+it is used to determine the current rank a player should be. */
 @ranks = { 
-
+	/* Base Ranks */
 	"Wood" 		=>	 "0"	, #0		
 	"Stone" 	=> 	"10"	, #10		
 	"Iron" 		=> 	"20"	, #10		
@@ -13,7 +15,8 @@ require 'RubyMinecraft'
 	"Diamond" 	=>  "160" 	, #40	
 	"Obsidian" 	=>  "200" 	, #40	
 	"Bedrock" 	=>  "250" 	, #50	
-
+	
+	/* First Prestige */
 	"Wood+"		=>	"300"	, #50	
 	"Stone+"	=>	"320"	, #20	
 	"Iron+"		=>	"340"	, #20	
@@ -24,7 +27,8 @@ require 'RubyMinecraft'
 	"Diamond+"	=>	"530"	, #50	
 	"Obsidian+"	=>	"580"	, #50	
 	"Bedrock+"	=>	"640"	, #60	
-
+	
+	/* Second Prestige */
 	"Wood++"	=>	"700"	, #60	
 	"Stone++"	=>	"730"	, #30	
 	"Iron++"	=>	"760"	, #30	
@@ -35,7 +39,8 @@ require 'RubyMinecraft'
 	"Diamond++"	=>	"990"	, #60	
 	"Obsidian++"=>	"1050"	, #60	
 	"Bedrock++"	=>	"1120"	, #70	
-
+	
+	/* Third Prestige */
 	"Wood+++"	=>	"1190"	, #70	
 	"Stone+++"	=>	"1230"	, #40	
 	"Iron+++"	=>	"1270"	, #40	
@@ -50,19 +55,31 @@ require 'RubyMinecraft'
 
 }
 
+# Ran when ranks need to be updated
 def doRanks()
 
 	Thread.new{
 		begin
+		
+		# Minecraft.new(Server IP as string, RCON port);
 		rcon = RCON::Minecraft.new("rr3.re-renderreality.net", 25576);
 		rcon.auth($CONFIG['rconpass'])
-
+		
+		# Query the database for player play times
 		time_players = @db.execute("SELECT TIMEPLAYED, MCUN FROM players")
+		
+		# Cycle through every player checking time played
 		time_players.each do |seconds, user|
-
+		
+			# x = hours player has played
 			x = (seconds / 3600.0)
+			
 			new_rank = nil
-
+			
+			/*  If player's hours played is less than the current
+				rank being checked, move on to the next once
+				we find a rank where they have more hours then the rank
+				requires, set new_rank to the rank they have more hours than */
 			catch :exit do
 				@ranks.each do |rank, hrs|
 					if x < hrs.to_f
@@ -72,27 +89,35 @@ def doRanks()
 					end
 				end
 			end
+			
+			# Gets the current rank of the player and then formats it properly
 			old_rank = @db.execute("SELECT RANK FROM players WHERE MCUN = ?", [user])
-
 			old_rank = old_rank.join("")
 			old_rank.delete!("[[\"")
 			old_rank.delete!("\"]]")
 
+			# Inputs staff list and converts it to a JSON object.
 			file = File.read('staff.json')
 			staff = JSON.parse(file)
 			
-			#check for not staff
+			# Check if player is not staff.
 			if staff['Staff'].include?(user)
 				nil
 			else
+				/* If the rank that the player's play time coresponds to is not their current rank,
+				set their current rank in the database and on the server to the new rank we determined above. */
 				if old_rank.to_s != new_rank.to_s
+					# Sets the new rank in the database
 					@db.execute("UPDATE players SET RANK = ? WHERE MCUN = ?", [new_rank,user])
+					# Sets the new rank on the server
 					rcon.command("p user " + user.to_s + " group set " + new_rank.to_s)
+					# Print the rank change to the debug console
 					p "p user " + user.to_s + " group set " + new_rank.to_s
 				end
 			end
 		end
 		nil
+	# Catches errors and prints them to the debug console
 	rescue => e 
 		p e
 	end
